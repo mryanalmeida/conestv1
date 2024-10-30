2991184b98a2a8d4c95fb3c12ef182f31e0ff98d
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-10-30 20:33:02 -0300

    conestv1

--- main.js
+++ main.js
@@ -1,29 +1,92 @@
-const { app, BrowserWindow } = require('electron');
+const { shell } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('node:path')
 
-//janela pricipal
+//Janela Principal
 function createWindow() {
-    nativeTheme.themeSource = 'dark'
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1010,
         height: 720,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    });
+    })
+    //Menu Personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-    win.loadFile('./src/views/index.html');
+    win.loadFile('./src/views/index.html')
 }
+
 
 //Janela sobre
 function aboutWindow() {
-    nativeTheme.themeSource = 'dark'
     const about = new BrowserWindow({
         width: 360,
         height: 220,
-        autoHideMenuBar: true, //esonder o menu
+        autoHideMenuBar: true,
         resizable: false,
         minimizable: false,
-        //titleBarStyle: 'hidden' //para totem de auto atendimento
     })
     about.loadFile('./src/views/sobre.html')
 }
+
+//Execução assincrona do aplicativo electron
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+//Template do menu
+const template = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Sair',
+                accelerator: 'Alt+F4',
+                click: () => app.quit()
+            }
+
+        ]
+    },
+    {
+        label: 'Zoom',
+        submenu: [
+            {
+                label: 'Aplicar Zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrao',
+                role: 'resetZoom'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Repositório',
+                click: () => shell.openExternal('https://github.com/mryanalmeida/conestv1')
+            },
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
