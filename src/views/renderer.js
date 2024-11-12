fa6ec7e5dbf209d072a5596a194ef030453320c4
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-11-11 22:01:50 -0300

    minidev

--- src/views/renderer.js
+++ src/views/renderer.js
@@ -22,3 +22,16 @@ function produtos() {
 function relatorios() {
     api.janelaRelatorios()
 }
+
+//inserção da data no rodapé
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+document.getElementById('dataAtual').innerHTML = obterData()
